bd811347a013442854ee94d179a52f4bb3a413f7
James Campbell <james.p.campbell@gmail.com>
2022-02-15 13:57:52 -0500

    Update changes in value set implementation

--- great_expectations/expectations/metrics/column_map_metrics/column_values_in_set.py
+++ great_expectations/expectations/metrics/column_map_metrics/column_values_in_set.py
@@ -1,4 +1,5 @@
 import warnings
+from typing import Dict, Any
 
 import numpy as np
 
@@ -7,6 +8,8 @@ from great_expectations.execution_engine import (
     SparkDFExecutionEngine,
     SqlAlchemyExecutionEngine,
 )
+from great_expectations.execution_engine.execution_engine import MetricPartialFunctionTypes, MetricDomainTypes
+from great_expectations.expectations.metrics import metric_partial
 from great_expectations.expectations.metrics.import_manager import F
 from great_expectations.expectations.metrics.map_metric_provider import (
     ColumnMapMetricProvider,
@@ -71,16 +74,23 @@ future release.  Please update code accordingly.  Moreover, in "{cls.__name__}._
 
         return column.in_(value_set)
 
-    @column_condition_partial(engine=SparkDFExecutionEngine)
+
+    @metric_partial(
+        engine=SparkDFExecutionEngine,
+        partial_fn_type=MetricPartialFunctionTypes.MAP_CONDITION_SERIES,
+        domain_type=MetricDomainTypes.COLUMN
+    )
     def _spark(
         cls,
-        column,
-        value_set,
-        **kwargs,
+        execution_engine: "SqlAlchemyExecutionEngine",
+        metric_domain_kwargs: Dict,
+        metric_value_kwargs: Dict,
+        metrics: Dict[str, Any],
+        runtime_configuration: Dict,
     ):
         # no need to parse as datetime; just compare the strings as is
         parse_strings_as_datetimes: bool = (
-            kwargs.get("parse_strings_as_datetimes") or False
+            metric_value_kwargs.get("parse_strings_as_datetimes") or False
         )
         if parse_strings_as_datetimes:
             warnings.warn(
@@ -90,8 +100,29 @@ future release.  Please update code accordingly.  Moreover, in "{cls.__name__}._
                 DeprecationWarning,
             )
 
+        value_set = metric_value_kwargs.get("value_set")
         if value_set is None:
             # vacuously true
             return F.lit(True)
 
-        return column.isin(value_set)
+        import pyspark
+        value_set_df = pyspark.sql.DataFrame({"value_set": value_set})
+        filter_column_isnull = metric_value_kwargs.get(
+            "filter_column_isnull", getattr(cls, "filter_column_isnull", True)
+        )
+
+        (
+            data,
+            compute_domain_kwargs,
+            accessor_domain_kwargs,
+        ) = execution_engine.get_compute_domain(
+            domain_kwargs=metric_domain_kwargs, domain_type=MetricDomainTypes.COLUMN
+        )
+        if filter_column_isnull:
+            data = data.filter(data[accessor_domain_kwargs["column"]].isNotNull())
+
+        res = data.join(value_set_df, data[accessor_domain_kwargs["column"]] == value_set_df["value_set"], "left")
+        # res = res.withColumn("__success", pyspark.sql.when(value_set_df["value_set"] == None, False).otherwise(True))
+        return pyspark.sql.when(value_set_df["value_set"] == None, 0).otherwise(1)
+
+        #  return column.isin(value_set)
