6c0342824df1551cea42c83884b98c4e19f3887f
Shinnnyshinshin <will@superconductive.com>
2022-05-26 16:55:49 -0700

    first push

--- great_expectations/data_context/data_context/abstract_data_context.py
+++ great_expectations/data_context/data_context/abstract_data_context.py
@@ -1,4 +1,7 @@
 from abc import ABC
+from typing import Mapping, Optional, Union
+
+from great_expectations.data_context.types.base import DataContextConfig
 
 
 class AbstractDataContext(ABC):
@@ -11,4 +14,179 @@ class AbstractDataContext(ABC):
     TODO: eventually the dependency on ConfigPeer will be removed and this will become a pure ABC.
     """
 
-    pass
+    PROFILING_ERROR_CODE_TOO_MANY_DATA_ASSETS = 2
+    PROFILING_ERROR_CODE_SPECIFIED_DATA_ASSETS_NOT_FOUND = 3
+    PROFILING_ERROR_CODE_NO_BATCH_KWARGS_GENERATORS_FOUND = 4
+    PROFILING_ERROR_CODE_MULTIPLE_BATCH_KWARGS_GENERATORS_FOUND = 5
+
+    DOLLAR_SIGN_ESCAPE_STRING = r"\$"
+
+    TEST_YAML_CONFIG_SUPPORTED_STORE_TYPES = [
+        "ExpectationsStore",
+        "ValidationsStore",
+        "HtmlSiteStore",
+        "EvaluationParameterStore",
+        "MetricStore",
+        "SqlAlchemyQueryStore",
+        "CheckpointStore",
+        "ProfilerStore",
+    ]
+    TEST_YAML_CONFIG_SUPPORTED_DATASOURCE_TYPES = [
+        "Datasource",
+        "SimpleSqlalchemyDatasource",
+    ]
+    TEST_YAML_CONFIG_SUPPORTED_DATA_CONNECTOR_TYPES = [
+        "InferredAssetFilesystemDataConnector",
+        "ConfiguredAssetFilesystemDataConnector",
+        "InferredAssetS3DataConnector",
+        "ConfiguredAssetS3DataConnector",
+        "InferredAssetAzureDataConnector",
+        "ConfiguredAssetAzureDataConnector",
+        "InferredAssetGCSDataConnector",
+        "ConfiguredAssetGCSDataConnector",
+        "InferredAssetSqlDataConnector",
+        "ConfiguredAssetSqlDataConnector",
+    ]
+    TEST_YAML_CONFIG_SUPPORTED_CHECKPOINT_TYPES = [
+        "Checkpoint",
+        "SimpleCheckpoint",
+    ]
+    TEST_YAML_CONFIG_SUPPORTED_PROFILER_TYPES = [
+        "RuleBasedProfiler",
+    ]
+    ALL_TEST_YAML_CONFIG_DIAGNOSTIC_INFO_TYPES = [
+        "__substitution_error__",
+        "__yaml_parse_error__",
+        "__custom_subclass_not_core_ge__",
+        "__class_name_not_provided__",
+    ]
+    ALL_TEST_YAML_CONFIG_SUPPORTED_TYPES = (
+        TEST_YAML_CONFIG_SUPPORTED_STORE_TYPES
+        + TEST_YAML_CONFIG_SUPPORTED_DATASOURCE_TYPES
+        + TEST_YAML_CONFIG_SUPPORTED_DATA_CONNECTOR_TYPES
+        + TEST_YAML_CONFIG_SUPPORTED_CHECKPOINT_TYPES
+        + TEST_YAML_CONFIG_SUPPORTED_PROFILER_TYPES
+    )
+
+    def __init__(
+        self,
+        project_config: Union[DataContextConfig, Mapping],
+        runtime_environment: Optional[dict] = None
+        # context_root_dir do we keep this?
+    ):
+        self._project_config = project_config
+        self.runtime_environment = runtime_environment or {}
+
+    def _apply_global_config_overrides(self) -> None:
+        """
+        Checking global settings like usage_statistics and environment variables and applying them to
+        config associated with current BaseDataContext
+
+        Run as part of BaseDataContext __init__()
+        """
+        # check for global usage statistics opt out
+        validation_errors = {}
+
+        if self._check_global_usage_statistics_opt_out():
+            logger.info(
+                "Usage statistics is disabled globally. Applying override to project_config."
+            )
+            self.config.anonymous_usage_statistics.enabled = False
+
+        # check for global data_context_id
+        global_data_context_id = self._get_global_config_value(
+            environment_variable="GE_DATA_CONTEXT_ID",
+            conf_file_section="anonymous_usage_statistics",
+            conf_file_option="data_context_id",
+        )
+        if global_data_context_id:
+            data_context_id_errors = anonymizedUsageStatisticsSchema.validate(
+                {"data_context_id": global_data_context_id}
+            )
+            if not data_context_id_errors:
+                logger.info(
+                    "data_context_id is defined globally. Applying override to project_config."
+                )
+                self.config.anonymous_usage_statistics.data_context_id = (
+                    global_data_context_id
+                )
+            else:
+                validation_errors.update(data_context_id_errors)
+        # check for global usage_statistics url
+        global_usage_statistics_url = self._get_global_config_value(
+            environment_variable="GE_USAGE_STATISTICS_URL",
+            conf_file_section="anonymous_usage_statistics",
+            conf_file_option="usage_statistics_url",
+        )
+        if global_usage_statistics_url:
+            usage_statistics_url_errors = anonymizedUsageStatisticsSchema.validate(
+                {"usage_statistics_url": global_usage_statistics_url}
+            )
+            if not usage_statistics_url_errors:
+                logger.info(
+                    "usage_statistics_url is defined globally. Applying override to project_config."
+                )
+                self.config.anonymous_usage_statistics.usage_statistics_url = (
+                    global_usage_statistics_url
+                )
+            else:
+                validation_errors.update(usage_statistics_url_errors)
+        if validation_errors:
+            logger.warning(
+                "The following globally-defined config variables failed validation:\n{}\n\n"
+                "Please fix the variables if you would like to apply global values to project_config.".format(
+                    json.dumps(validation_errors, indent=2)
+                )
+            )
+
+    @classmethod
+    def _get_global_config_value(
+        cls,
+        environment_variable: Optional[str] = None,
+        conf_file_section=None,
+        conf_file_option=None,
+    ) -> Optional[str]:
+        assert (conf_file_section and conf_file_option) or (
+            not conf_file_section and not conf_file_option
+        ), "Must pass both 'conf_file_section' and 'conf_file_option' or neither."
+        if environment_variable and os.environ.get(environment_variable, False):
+            return os.environ.get(environment_variable)
+        if conf_file_section and conf_file_option:
+            for config_path in BaseDataContext.GLOBAL_CONFIG_PATHS:
+                config = configparser.ConfigParser()
+                config.read(config_path)
+                config_value = config.get(
+                    conf_file_section, conf_file_option, fallback=None
+                )
+                if config_value:
+                    return config_value
+        return None
+
+    @staticmethod
+    def _check_global_usage_statistics_opt_out() -> bool:
+        if os.environ.get("GE_USAGE_STATS", False):
+            ge_usage_stats = os.environ.get("GE_USAGE_STATS")
+            if ge_usage_stats in BaseDataContext.FALSEY_STRINGS:
+                return True
+            else:
+                logger.warning(
+                    "GE_USAGE_STATS environment variable must be one of: {}".format(
+                        BaseDataContext.FALSEY_STRINGS
+                    )
+                )
+        for config_path in BaseDataContext.GLOBAL_CONFIG_PATHS:
+            config = configparser.ConfigParser()
+            states = config.BOOLEAN_STATES
+            for falsey_string in BaseDataContext.FALSEY_STRINGS:
+                states[falsey_string] = False
+            states["TRUE"] = True
+            states["True"] = True
+            config.BOOLEAN_STATES = states
+            config.read(config_path)
+            try:
+                if config.getboolean("anonymous_usage_statistics", "enabled") is False:
+                    # If stats are disabled, then opt out is true
+                    return True
+            except (ValueError, configparser.Error):
+                pass
+        return False
--- great_expectations/data_context/data_context/base_data_context.py
+++ great_expectations/data_context/data_context/base_data_context.py
@@ -128,6 +128,7 @@ except ImportError:
     SQLAlchemyError = ge_exceptions.ProfilerError
 
 logger = logging.getLogger(__name__)
+# TODO update this
 yaml = YAML()
 yaml.indent(mapping=2, sequence=4, offset=2)
 yaml.default_flow_style = False
@@ -239,10 +240,12 @@ class BaseDataContext(EphemeralDataContext, ConfigPeer):
     --ge-feature-maturity-info--
     """
 
-    PROFILING_ERROR_CODE_TOO_MANY_DATA_ASSETS = 2
-    PROFILING_ERROR_CODE_SPECIFIED_DATA_ASSETS_NOT_FOUND = 3
-    PROFILING_ERROR_CODE_NO_BATCH_KWARGS_GENERATORS_FOUND = 4
-    PROFILING_ERROR_CODE_MULTIPLE_BATCH_KWARGS_GENERATORS_FOUND = 5
+    # PROFILING_ERROR_CODE_TOO_MANY_DATA_ASSETS = 2
+    # PROFILING_ERROR_CODE_SPECIFIED_DATA_ASSETS_NOT_FOUND = 3
+    # PROFILING_ERROR_CODE_NO_BATCH_KWARGS_GENERATORS_FOUND = 4
+    # PROFILING_ERROR_CODE_MULTIPLE_BATCH_KWARGS_GENERATORS_FOUND = 5
+
+    # Related to file: anything that uses these should be under FileDataContext
     UNCOMMITTED_DIRECTORIES = ["data_docs", "validations"]
     GE_UNCOMMITTED_DIR = "uncommitted"
     BASE_DIRECTORIES = [
@@ -255,63 +258,76 @@ class BaseDataContext(EphemeralDataContext, ConfigPeer):
     GE_DIR = "great_expectations"
     GE_YML = "great_expectations.yml"
     GE_EDIT_NOTEBOOK_DIR = GE_UNCOMMITTED_DIR
+
+    # this is the one that we refer to in this file
     FALSEY_STRINGS = ["FALSE", "false", "False", "f", "F", "0"]
+
+    # Related to file: anything that uses this should be under FileDataContext
     GLOBAL_CONFIG_PATHS = [
         os.path.expanduser("~/.great_expectations/great_expectations.conf"),
         "/etc/great_expectations.conf",
     ]
+
     DOLLAR_SIGN_ESCAPE_STRING = r"\$"
-    TEST_YAML_CONFIG_SUPPORTED_STORE_TYPES = [
-        "ExpectationsStore",
-        "ValidationsStore",
-        "HtmlSiteStore",
-        "EvaluationParameterStore",
-        "MetricStore",
-        "SqlAlchemyQueryStore",
-        "CheckpointStore",
-        "ProfilerStore",
-    ]
-    TEST_YAML_CONFIG_SUPPORTED_DATASOURCE_TYPES = [
-        "Datasource",
-        "SimpleSqlalchemyDatasource",
-    ]
-    TEST_YAML_CONFIG_SUPPORTED_DATA_CONNECTOR_TYPES = [
-        "InferredAssetFilesystemDataConnector",
-        "ConfiguredAssetFilesystemDataConnector",
-        "InferredAssetS3DataConnector",
-        "ConfiguredAssetS3DataConnector",
-        "InferredAssetAzureDataConnector",
-        "ConfiguredAssetAzureDataConnector",
-        "InferredAssetGCSDataConnector",
-        "ConfiguredAssetGCSDataConnector",
-        "InferredAssetSqlDataConnector",
-        "ConfiguredAssetSqlDataConnector",
-    ]
-    TEST_YAML_CONFIG_SUPPORTED_CHECKPOINT_TYPES = [
-        "Checkpoint",
-        "SimpleCheckpoint",
-    ]
-    TEST_YAML_CONFIG_SUPPORTED_PROFILER_TYPES = [
-        "RuleBasedProfiler",
-    ]
-    ALL_TEST_YAML_CONFIG_DIAGNOSTIC_INFO_TYPES = [
-        "__substitution_error__",
-        "__yaml_parse_error__",
-        "__custom_subclass_not_core_ge__",
-        "__class_name_not_provided__",
-    ]
-    ALL_TEST_YAML_CONFIG_SUPPORTED_TYPES = (
-        TEST_YAML_CONFIG_SUPPORTED_STORE_TYPES
-        + TEST_YAML_CONFIG_SUPPORTED_DATASOURCE_TYPES
-        + TEST_YAML_CONFIG_SUPPORTED_DATA_CONNECTOR_TYPES
-        + TEST_YAML_CONFIG_SUPPORTED_CHECKPOINT_TYPES
-        + TEST_YAML_CONFIG_SUPPORTED_PROFILER_TYPES
-    )
+    # TEST_YAML_CONFIG_SUPPORTED_STORE_TYPES = [
+    #     "ExpectationsStore",
+    #     "ValidationsStore",
+    #     "HtmlSiteStore",
+    #     "EvaluationParameterStore",
+    #     "MetricStore",
+    #     "SqlAlchemyQueryStore",
+    #     "CheckpointStore",
+    #     "ProfilerStore",
+    # ]
+    # TEST_YAML_CONFIG_SUPPORTED_DATASOURCE_TYPES = [
+    #     "Datasource",
+    #     "SimpleSqlalchemyDatasource",
+    # ]
+    # TEST_YAML_CONFIG_SUPPORTED_DATA_CONNECTOR_TYPES = [
+    #     "InferredAssetFilesystemDataConnector",
+    #     "ConfiguredAssetFilesystemDataConnector",
+    #     "InferredAssetS3DataConnector",
+    #     "ConfiguredAssetS3DataConnector",
+    #     "InferredAssetAzureDataConnector",
+    #     "ConfiguredAssetAzureDataConnector",
+    #     "InferredAssetGCSDataConnector",
+    #     "ConfiguredAssetGCSDataConnector",
+    #     "InferredAssetSqlDataConnector",
+    #     "ConfiguredAssetSqlDataConnector",
+    # ]
+    # TEST_YAML_CONFIG_SUPPORTED_CHECKPOINT_TYPES = [
+    #     "Checkpoint",
+    #     "SimpleCheckpoint",
+    # ]
+    # TEST_YAML_CONFIG_SUPPORTED_PROFILER_TYPES = [
+    #     "RuleBasedProfiler",
+    # ]
+    # ALL_TEST_YAML_CONFIG_DIAGNOSTIC_INFO_TYPES = [
+    #     "__substitution_error__",
+    #     "__yaml_parse_error__",
+    #     "__custom_subclass_not_core_ge__",
+    #     "__class_name_not_provided__",
+    # ]
+    # ALL_TEST_YAML_CONFIG_SUPPORTED_TYPES = (
+    #     TEST_YAML_CONFIG_SUPPORTED_STORE_TYPES
+    #     + TEST_YAML_CONFIG_SUPPORTED_DATASOURCE_TYPES
+    #     + TEST_YAML_CONFIG_SUPPORTED_DATA_CONNECTOR_TYPES
+    #     + TEST_YAML_CONFIG_SUPPORTED_CHECKPOINT_TYPES
+    #     + TEST_YAML_CONFIG_SUPPORTED_PROFILER_TYPES
+    # )
 
     _data_context = None
 
     @classmethod
     def validate_config(cls, project_config: Union[DataContextConfig, Mapping]) -> bool:
+        """
+        Class method to validate config against schema. Run as one of the first steps of BaseDataContext __init__()
+        Args:
+            project_config (DataContextConfig, Mapping): config to check
+
+        Returns:
+            Should return True if the configuration is valid. Error if not.
+        """
         if isinstance(project_config, DataContextConfig):
             return True
         try:
@@ -342,20 +358,23 @@ class BaseDataContext(EphemeralDataContext, ConfigPeer):
         Returns:
             None
         """
+        super().__init__(
+            project_config=project_config, runtime_environment=runtime_environment
+        )
         if not BaseDataContext.validate_config(project_config):
             raise ge_exceptions.InvalidConfigError(
                 "Your project_config is not valid. Try using the CLI check-config command."
             )
         self._ge_cloud_mode = ge_cloud_mode
         self._ge_cloud_config = ge_cloud_config
-        self._project_config = project_config
+        # self._project_config = project_config
         self._apply_global_config_overrides()
 
         if context_root_dir is not None:
             context_root_dir = os.path.abspath(context_root_dir)
         self._context_root_directory = context_root_dir
 
-        self.runtime_environment = runtime_environment or {}
+        # self.runtime_environment = runtime_environment or {}
 
         # Init plugin support
         if self.plugins_directory is not None and os.path.exists(
@@ -480,114 +499,6 @@ class BaseDataContext(EphemeralDataContext, ConfigPeer):
                 # this is ok, as long as we don't use it to retrieve a batch. If we try to do that, the error will be
                 # caught at the context.get_batch() step. So we just pass here.
                 pass
-
-    def _apply_global_config_overrides(self) -> None:
-        # check for global usage statistics opt out
-        validation_errors = {}
-
-        if self._check_global_usage_statistics_opt_out():
-            logger.info(
-                "Usage statistics is disabled globally. Applying override to project_config."
-            )
-            self.config.anonymous_usage_statistics.enabled = False
-
-        # check for global data_context_id
-        global_data_context_id = self._get_global_config_value(
-            environment_variable="GE_DATA_CONTEXT_ID",
-            conf_file_section="anonymous_usage_statistics",
-            conf_file_option="data_context_id",
-        )
-        if global_data_context_id:
-            data_context_id_errors = anonymizedUsageStatisticsSchema.validate(
-                {"data_context_id": global_data_context_id}
-            )
-            if not data_context_id_errors:
-                logger.info(
-                    "data_context_id is defined globally. Applying override to project_config."
-                )
-                self.config.anonymous_usage_statistics.data_context_id = (
-                    global_data_context_id
-                )
-            else:
-                validation_errors.update(data_context_id_errors)
-        # check for global usage_statistics url
-        global_usage_statistics_url = self._get_global_config_value(
-            environment_variable="GE_USAGE_STATISTICS_URL",
-            conf_file_section="anonymous_usage_statistics",
-            conf_file_option="usage_statistics_url",
-        )
-        if global_usage_statistics_url:
-            usage_statistics_url_errors = anonymizedUsageStatisticsSchema.validate(
-                {"usage_statistics_url": global_usage_statistics_url}
-            )
-            if not usage_statistics_url_errors:
-                logger.info(
-                    "usage_statistics_url is defined globally. Applying override to project_config."
-                )
-                self.config.anonymous_usage_statistics.usage_statistics_url = (
-                    global_usage_statistics_url
-                )
-            else:
-                validation_errors.update(usage_statistics_url_errors)
-        if validation_errors:
-            logger.warning(
-                "The following globally-defined config variables failed validation:\n{}\n\n"
-                "Please fix the variables if you would like to apply global values to project_config.".format(
-                    json.dumps(validation_errors, indent=2)
-                )
-            )
-
-    @classmethod
-    def _get_global_config_value(
-        cls,
-        environment_variable: Optional[str] = None,
-        conf_file_section=None,
-        conf_file_option=None,
-    ) -> Optional[str]:
-        assert (conf_file_section and conf_file_option) or (
-            not conf_file_section and not conf_file_option
-        ), "Must pass both 'conf_file_section' and 'conf_file_option' or neither."
-        if environment_variable and os.environ.get(environment_variable, False):
-            return os.environ.get(environment_variable)
-        if conf_file_section and conf_file_option:
-            for config_path in BaseDataContext.GLOBAL_CONFIG_PATHS:
-                config = configparser.ConfigParser()
-                config.read(config_path)
-                config_value = config.get(
-                    conf_file_section, conf_file_option, fallback=None
-                )
-                if config_value:
-                    return config_value
-        return None
-
-    @staticmethod
-    def _check_global_usage_statistics_opt_out() -> bool:
-        if os.environ.get("GE_USAGE_STATS", False):
-            ge_usage_stats = os.environ.get("GE_USAGE_STATS")
-            if ge_usage_stats in BaseDataContext.FALSEY_STRINGS:
-                return True
-            else:
-                logger.warning(
-                    "GE_USAGE_STATS environment variable must be one of: {}".format(
-                        BaseDataContext.FALSEY_STRINGS
-                    )
-                )
-        for config_path in BaseDataContext.GLOBAL_CONFIG_PATHS:
-            config = configparser.ConfigParser()
-            states = config.BOOLEAN_STATES
-            for falsey_string in BaseDataContext.FALSEY_STRINGS:
-                states[falsey_string] = False
-            states["TRUE"] = True
-            states["True"] = True
-            config.BOOLEAN_STATES = states
-            config.read(config_path)
-            try:
-                if config.getboolean("anonymous_usage_statistics", "enabled") is False:
-                    # If stats are disabled, then opt out is true
-                    return True
-            except (ValueError, configparser.Error):
-                pass
-        return False
 
     def _construct_data_context_id(self) -> str:
         """
