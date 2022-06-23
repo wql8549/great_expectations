c66e5aced468f6a1dea26606fd8ab7df7a636c77
Anthony Burdi <anthony@superconductive.com>
2022-06-23 17:29:25 -0400

    WIP

--- great_expectations/datasource/data_connector/data_connector_converter.py
+++ great_expectations/datasource/data_connector/data_connector_converter.py
@@ -1,9 +1,22 @@
+from typing import List, Optional, Union
+
+from great_expectations.core import ExpectationSuiteValidationResult
+from great_expectations.data_context.types.base import (
+    DataConnectorConfig,
+    dataConnectorConfigSchema,
+)
+from great_expectations.datasource.data_connector import (
+    InferredAssetFilePathDataConnector,
+    InferredAssetSqlDataConnector,
+)
+
+
 class InferredToConfiguredDataConnectorConverter:
     pass
 
     INFERRED_TO_CONFIGURED_MAPPING: dict = {
         "InferredAssetAzureDataConnector": "ConfiguredAssetAzureDataConnector",
-        # TODO: Fill this in
+        # TODO: AJB 20220623 Fill this in
     }
 
     def convert(
@@ -13,10 +26,91 @@ class InferredToConfiguredDataConnectorConverter:
             InferredAssetSqlDataConnector,
             DataConnectorConfig,
         ],
+        validation_results: Optional[List[ExpectationSuiteValidationResult]] = None,
     ):
+
+        # TODO: AJB 20220623 Use the INFERRED_TO_CONFIGURED_MAPPING mapping here
+        if isinstance(inferred_data_connector, InferredAssetSqlDataConnector):
+            return self._convert_inferred_sql(
+                inferred_data_connector, validation_results
+            )
         """Placeholder method for testing conversion to ConfiguredAsset"""
-        # TODO: This should be broken up into separate methods for data connector type
-        # TODO: This or related methods should check to make sure the data connector is not a custom user created one
+        # TODO: AJB 20220623 This should be broken up into separate methods for data connector type
+        # TODO: AJB 20220623 This or related methods should check to make sure the data connector is not a custom user created one
         pass
 
-    # TODO: What methods are needed here?
+    # TODO: AJB 20220623 What methods are needed here?
+
+    def _get_asset_names_from_validation_results(
+        self,
+        data_connector_name: str,
+        validation_results: Optional[List[ExpectationSuiteValidationResult]] = None,
+    ) -> List[str]:
+        # TODO: AJB 20220623 Add docstring
+        data_asset_names: List[str] = []
+
+        # Skip if we can't get the data asset name
+        for validation_result in validation_results:
+            if "meta" in validation_result:
+                if "active_batch_definition" in validation_result["meta"]:
+                    if (
+                        "data_asset_name"
+                        in validation_result["meta"]["active_batch_definition"]
+                    ):
+                        # Only add for assets under this data connector
+                        if (
+                            validation_result["meta"]["active_batch_definition"][
+                                "data_connector_name"
+                            ]
+                            == data_connector_name
+                        ):
+                            data_asset_names.append(
+                                validation_result["meta"]["active_batch_definition"][
+                                    "data_asset_name"
+                                ]
+                            )
+
+        return data_asset_names
+
+    def _convert_inferred_sql(
+        self,
+        inferred_data_connector: Union[
+            InferredAssetFilePathDataConnector,
+            InferredAssetSqlDataConnector,
+            DataConnectorConfig,
+        ],
+        validation_results: Optional[List[ExpectationSuiteValidationResult]] = None,
+    ):
+        # TODO: AJB 20220623 Flesh this docstring out
+        """Convert an InferredAssetSqlDataConnector to a ConfiguredAssetSqlDataConnector."""
+
+        data_asset_names: List[str] = []
+        # TODO: AJB 20220623 Do we try accessing data to get asset names if it is available? YES. If so split into separate method.
+        # try:
+        #     asset_names_from_db: List[str] = inferred_data_connector.get_available_data_asset_names()
+        #     data_asset_names.extend(asset_names_from_db)
+        # except: # TODO: AJB 20220623 What is the right exception type for this?
+        #     pass
+
+        data_asset_names.extend(
+            self._get_asset_names_from_validation_results(
+                inferred_data_connector.name, validation_results
+            )
+        )
+
+        assets = {data_asset_name: {} for data_asset_name in data_asset_names}
+
+        configured_asset_data_connector_config: DataConnectorConfig = (
+            DataConnectorConfig(
+                name=f"auto_generated_from_{inferred_data_connector.name}",
+                class_name="ConfiguredAssetSqlDataConnector",
+                module_name="great_expectations.datasource.data_connector",
+                datasource_name=inferred_data_connector.datasource_name,
+                assets=assets,
+            )
+        )
+        # TODO: AJB 20220623 is a round trip through the schema necessary?:
+        # validated_config: DataConnectorConfig = dataConnectorConfigSchema.load(configured_asset_data_connector_config.to_json_dict())
+        # return validated_config
+
+        return configured_asset_data_connector_config
--- tests/datasource/data_connector/test_data_connector_converter.py
+++ tests/datasource/data_connector/test_data_connector_converter.py
@@ -1,14 +1,23 @@
+import os
+from pathlib import Path
 from typing import List
 
 import pandas as pd
 
 from great_expectations import DataContext
 from great_expectations.checkpoint import Checkpoint
+from great_expectations.checkpoint.types.checkpoint_result import CheckpointResult
 from great_expectations.core import (
     ExpectationConfiguration,
     ExpectationSuiteValidationResult,
 )
-from great_expectations.core.batch import RuntimeBatchRequest
+from great_expectations.core.batch import BatchRequest, RuntimeBatchRequest
+from great_expectations.data_context.types.base import DataConnectorConfig
+from great_expectations.datasource import BaseDatasource, DataConnector
+from great_expectations.datasource.data_connector import InferredAssetSqlDataConnector
+from great_expectations.datasource.data_connector.data_connector_converter import (
+    InferredToConfiguredDataConnectorConverter,
+)
 
 
 def test_retrieve_asset_from_validation_result_file_based(
@@ -16,7 +25,7 @@ def test_retrieve_asset_from_validation_result_file_based(
 ):
     """Investigatory happy path test retrieving asset info from a validation result."""
 
-    # TODO: Use a more purpose built fixture
+    # TODO: AJB 20220623 Use a more purpose built fixture
     context: DataContext = titanic_pandas_data_context_with_v013_datasource_with_checkpoints_v1_with_empty_store_stats_enabled
 
     test_df: pd.DataFrame = pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4]})
@@ -348,8 +357,212 @@ def test_retrieve_asset_from_validation_result_file_based(
     # ]
 
 
-def test_retrieve_asset_from_validation_result_sql_based():
-    pass
+def _create_and_add_datasource_to_context(context: DataContext) -> DataContext:
+
+    # TODO: AJB 20220623 Clean this up, add params and follow this pattern for other steps
+
+    # Create and add datasource
+    test_sets_path: str = os.path.join(Path(__file__).parents[2], "test_sets")
+    db_file_abspath: str = os.path.join(
+        test_sets_path, "test_cases_for_sql_data_connector.db"
+    )
+    assert os.path.exists(db_file_abspath)
+
+    datasource_name: str = "testing_datasource_name"
+    data_connector_name: str = "testing_data_connector_name"
+
+    datasource_config = {
+        "name": datasource_name,
+        "class_name": "Datasource",
+        "execution_engine": {
+            "class_name": "SqlAlchemyExecutionEngine",
+            "connection_string": f"sqlite:///{db_file_abspath}",
+        },
+        "data_connectors": {
+            data_connector_name: {
+                "class_name": "InferredAssetSqlDataConnector",
+                "include_schema_name": True,
+            },
+        },
+    }
+    context.add_datasource(**datasource_config)
+
+    assert len(context.list_datasources()) == 1
+
+    # Check datasource
+    datasource: BaseDatasource = context.get_datasource(datasource_name)
+    data_connector: DataConnector = datasource.data_connectors[data_connector_name]
+    assert data_connector.get_available_data_asset_names() == [
+        "main.table_containing_id_spacers_for_D",
+        "main.table_full__I",
+        "main.table_partitioned_by_date_column__A",
+        "main.table_partitioned_by_foreign_key__F",
+        "main.table_partitioned_by_incrementing_batch_id__E",
+        "main.table_partitioned_by_irregularly_spaced_incrementing_id_with_spacing_in_a_second_table__D",
+        "main.table_partitioned_by_multiple_columns__G",
+        "main.table_partitioned_by_regularly_spaced_incrementing_id_column__C",
+        "main.table_partitioned_by_timestamp_column__B",
+        "main.table_that_should_be_partitioned_by_random_hash__H",
+        "main.table_with_fk_reference_from_F",
+        "main.view_by_date_column__A",
+        "main.view_by_incrementing_batch_id__E",
+        "main.view_by_irregularly_spaced_incrementing_id_with_spacing_in_a_second_table__D",
+        "main.view_by_multiple_columns__G",
+        "main.view_by_regularly_spaced_incrementing_id_column__C",
+        "main.view_by_timestamp_column__B",
+        "main.view_containing_id_spacers_for_D",
+        "main.view_partitioned_by_foreign_key__F",
+        "main.view_that_should_be_partitioned_by_random_hash__H",
+        "main.view_with_fk_reference_from_F",
+    ]
+    return context
+
+
+def _generate_sql_validation_results_and_update_context(
+    context: DataContext,
+) -> List[ExpectationSuiteValidationResult]:
+    # TODO: AJB 20220623 Insert docstring
+    # TODO: AJB 20220623 Split up this method
+
+    # Init data context
+    # Set up data connector
+    # Set up batch request
+    # Set up checkpoint
+    # Set up expectation suite
+    # Run checkpoint
+    # Get validation result
+
+    datasource_name: str = "testing_datasource_name"
+    data_connector_name: str = "testing_data_connector_name"
+    context = _create_and_add_datasource_to_context(context)
+
+    # Create and add Expectation Suite
+    expectation_suite_name: str = "testing_expectation_suite"
+    context.create_expectation_suite(expectation_suite_name)
+    suite = context.get_expectation_suite(expectation_suite_name)
+    # This suite will purposely fail
+    expectation_configuration = ExpectationConfiguration(
+        expectation_type="expect_table_columns_to_match_ordered_list",
+        kwargs={
+            "column_list": [
+                "account_id",
+                "user_id",
+                "transaction_id",
+                "transaction_type",
+                "transaction_amt_usd",
+            ]
+        },
+        meta={
+            "notes": {
+                "format": "markdown",
+                "content": "Some clever comment about this expectation. **Markdown** `Supported`",
+            }
+        },
+    )
+    suite.add_expectation(expectation_configuration=expectation_configuration)
+    context.save_expectation_suite(expectation_suite=suite)
+
+    assert context.list_expectation_suite_names() == [expectation_suite_name]
+    assert len(context.get_expectation_suite(expectation_suite_name).expectations) == 1
+
+    # Create Checkpoint
+    checkpoint_name: str = "testing_checkpoint_name"
+
+    checkpoint: Checkpoint = Checkpoint(
+        name=checkpoint_name,
+        data_context=context,
+        config_version=1,
+        run_name_template="%Y-%M-foo-bar-template",
+        expectation_suite_name=expectation_suite_name,
+        action_list=[
+            {
+                "name": "store_validation_result",
+                "action": {
+                    "class_name": "StoreValidationResultAction",
+                },
+            },
+            {
+                "name": "store_evaluation_params",
+                "action": {
+                    "class_name": "StoreEvaluationParametersAction",
+                },
+            },
+            # {
+            #     "name": "update_data_docs",
+            #     "action": {
+            #         "class_name": "UpdateDataDocsAction",
+            #     },
+            # },
+        ],
+    )
+
+    # Create Batch Request
+    data_asset_name: str = "main.table_partitioned_by_date_column__A"
+    batch_request: BatchRequest = BatchRequest(
+        datasource_name, data_connector_name, data_asset_name
+    )
+
+    checkpoint_result: CheckpointResult = checkpoint.run(
+        validations=[
+            {"batch_request": batch_request},
+        ]
+    )
+
+    validation_keys: List[str] = context.validations_store.list_keys()
+
+    assert len(validation_keys) == 1
+    assert not checkpoint_result["success"]
+
+    # print(context.validations_store.get(validation_keys[0]))
+
+    validation_results: List[ExpectationSuiteValidationResult] = []
+
+    for validation_key in validation_keys:
+        validation_result = context.validations_store.get(validation_key)
+        validation_results.append(validation_result)
+        # print(type(validation_result))  # <class 'great_expectations.core.expectation_validation_result.ExpectationSuiteValidationResult'>
+        # print(validation_result)
+
+    return validation_results
+
+
+def test_retrieve_asset_from_validation_result_sql_based(empty_data_context):
+
+    context: DataContext = empty_data_context
+    validation_results: List[
+        ExpectationSuiteValidationResult
+    ] = _generate_sql_validation_results_and_update_context(context)
+
+    # TODO: AJB 20220623 Validation result as fixture instead of generated?
+
+    data_connector_converter: InferredToConfiguredDataConnectorConverter = (
+        InferredToConfiguredDataConnectorConverter()
+    )
+
+    # TODO: AJB 20220623 Enum for test names
+    datasource_name: str = "testing_datasource_name"
+    data_connector_name: str = "testing_data_connector_name"
+
+    datasource: BaseDatasource = context.get_datasource(datasource_name)
+    inferred_data_connector: InferredAssetSqlDataConnector = datasource.data_connectors[
+        data_connector_name
+    ]
+
+    # Use validation result to create configured asset
+    # TODO: AJB 20220623 should this be the configs and not the instantiated connectors?
+    configured_asset_data_connector_config: DataConnectorConfig = (
+        data_connector_converter.convert(inferred_data_connector, validation_results)
+    )
+
+    # Assertions
+
+    assert configured_asset_data_connector_config.to_json_dict() == {
+        "class_name": "ConfiguredAssetSqlDataConnector",
+        "module_name": "great_expectations.datasource.data_connector",
+        "name": "auto_generated_from_testing_data_connector_name",
+        "datasource_name": "testing_datasource_name",
+        "assets": {"main.table_partitioned_by_date_column__A": {}},
+    }
 
 
 def test_retrieve_asset_from_checkpoint_configuration():
